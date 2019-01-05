0505e9a7fd69ca47965b45ce4a67851de01c0603
epatsioti <t8170106@dias.aueb.gr>
2019-01-05 20:30:57 +0200

    Update Insertion.java
    not completed

--- dbproject/src/main/java/javameister/dbproject/Insertion.java
+++ dbproject/src/main/java/javameister/dbproject/Insertion.java
@@ -1,40 +1,43 @@
 package javameister.dbproject;
-import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.util.*;
 
 class Insertion {
+	private static int counter = 0;
+	private static int code = 0;
 	
-	public static int getNumOfAtts() {
-		System.out.println("---Welcome to our first Data Base---");
-		System.out.println("You can store 10 attributes max for each element");
-		
-		Scanner input = new Scanner(System.in);
-		int number = 0;
-		
-		do {
-		//while (number > 10 || number < 1) {
-			try {
-				System.out.print("Please enter the num of attributes you want:");
-				number = input.nextInt();
-			} catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				System.out.println("Progam will be terminated");
-				number = 0 ;
-				System.exit(number);
-				
-			}
-				
-			if (number > 10 || number < 0) {
-				System.out.println("Number of attributes must be between 1 and 10");
-			}
-		//}
-				
-			
-		} while (number > 10 || number < 1);
-		input.close();
-		
-		return number;
+	public static void test() {
+		System.out.println( "this is our insertion class" );
 	}
 	
-
+	/**
+	 * 
+	 * recursive method that takes input from user in order to create the database
+	 */
+	public static void insertData() {
+		
+		Scanner input = new Scanner(System.in);
+		if (counter == 0) {
+			//columnsName();
+			counter += 1;
+		}
+		String element = null;
+		ArrayList<String> a = new ArrayList<String>();
+		
+		System.out.println("Insert your data.");
+		System.out.println("Press enter to enter the next element of this line.");
+		System.out.println("Type nextline to change line and endofinput in order to end inserting data.");
+		
+		code += 1;
+		a.add(" "+code);
+		do {
+			element = input.nextLine();
+			if (!element.equals("nextline") && !element.equals("endofinput")) {
+				a.add(element);
+			}
+		} while (!element.equals("nextline") && !element.equals("endofinput"));
+		//new Database(a);
+		if (element.equals("nextline")) {
+			insertData();
+		}
+	}
 }
