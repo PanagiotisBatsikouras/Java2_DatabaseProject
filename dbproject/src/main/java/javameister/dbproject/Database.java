1a236277221b42bb1e2026038cba99176c0fc512
PanagiotisBatsikouras <t8160079@dias.aueb.gr>
2019-01-05 22:56:43 +0200

    Basic Database class, needs printing methods
    the class contains the basic constructors, setters and getters in order to complete the basic use cases.

--- dbproject/src/main/java/javameister/dbproject/Database.java
+++ dbproject/src/main/java/javameister/dbproject/Database.java
@@ -1,5 +1,39 @@
 package javameister.dbproject;
+import java.util.*;
 
 class Database {
+	
+	private static ArrayList<String> row = new ArrayList<String>();
+	
+	private static ArrayList<ArrayList<String>> aList = new ArrayList<ArrayList<String>>();
+	
+	
+	public Database() {
+		
+	}
+	
+	public Database(ArrayList<String> row) {
+		this.row = row;
+		aList.add(this.row);
+		
+	}
+	
+	
+	public static ArrayList<String> getRow() {
+		return row;
+	}
+
+	public void setRow(ArrayList<String> row) {
+		this.row = row;
+	}
+
+	public static ArrayList<ArrayList<String>> getaList() {
+		return aList;
+	}
+
+	public void setaList(ArrayList<ArrayList<String>> aList) {
+		this.aList = aList;
+	}
+	
 
 }
