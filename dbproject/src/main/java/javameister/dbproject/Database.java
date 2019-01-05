846d91d50b3bedbcd77128762e93fa12b1794aed
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2019-01-05 23:51:33 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/PanagiotisBatsikouras/Java2_DatabaseProject""
    This reverts commit 3b5405721abd8a68a1efebaac61629b747e18c53.

--- dbproject/src/main/java/javameister/dbproject/Database.java
+++ dbproject/src/main/java/javameister/dbproject/Database.java
@@ -1,5 +1,44 @@
 package javameister.dbproject;
+import java.util.*;
 
 class Database {
+	
+	private static ArrayList<String> row = new ArrayList<String>();
+	
+	private static ArrayList<ArrayList<String>> aList = new ArrayList<ArrayList<String>>();
+	
+	
+	public Database() {
+		
+	}
+	
+	public Database(ArrayList<String> rowIn) {
+		row = rowIn;
+		aList.add(row);
+		
+	}
+	
+	
+	public static ArrayList<String> getRow() {
+		return row;
+	}
+
+	/*useless method. still has warning*/
+	/*
+	public void setRow(ArrayList<String> rowIn) {
+		row = rowIn;
+	}*/
+
+	public static ArrayList<ArrayList<String>> getaList() {
+		return aList;
+	}
+	
+	/*useless method, still has warning*/
+	/*
+	public void setaList(ArrayList<ArrayList<String>> aListIn) {
+		aList = aListIn;
+	}
+	*/
+	
 
 }
