abade19ce327ff73adb212cd1aa87eb1bbbd78ea
epatsioti <t8170106@dias.aueb.gr>
2019-01-05 19:10:00 +0200

    Update App.java

--- dbproject/src/main/java/javameister/dbproject/App.java
+++ dbproject/src/main/java/javameister/dbproject/App.java
@@ -1,13 +1,70 @@
 package javameister.dbproject;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
- * Hello world!
+ * main class for program execution
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main( String[] args ) {
+		System.out.println("Welcome to Javamaister Database");
+		try {
+			getMenu();
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	
+	/**
+	 *  method that prints basic menu for user
+	 *  the method reads user choice for the basic menu
+	 *  and calls the appropriate methods from the other 
+	 */
+	public static void getMenu() {
+		Scanner input = new Scanner(System.in);
+		boolean flag = true;
+		int choice;
+		try {
+			do {
+				System.out.println();
+				System.out.println("***Menu***");
+				System.out.println("1. Insert Data");
+				System.out.println("2. Display Data");
+				System.out.println("3. Delete Data");
+				System.out.println("4. Change Data");
+				System.out.println("5. Exit Database");
+				System.out.println("Make your choice: ");
+				choice = input.nextInt() ;
+				System.out.println();
+				switch(choice) {
+				case 1 :
+					//Insertion.insertData();
+					break;
+				case 2 :
+					//Printing.displayData();
+					break;
+				case 3 :
+					//Deletion.deleteData();
+					break;
+				case 4 :
+					//Modification.changeData();
+					break;
+				case 5 :
+					flag = false;
+					System.out.println("Thank you for using our database! Goodbye!");
+					break;
+				default:
+					System.out.println("Invalid choice");
+					break;
+				}
+			} while(flag);
+		} catch (InputMismatchException e) {
+			System.out.println("An error occurred during your input. The program will be terminated. Sorry for any inconvenience.");
+		} finally  {
+			input.close();
+		}
+			input.close();
+	}
 }
