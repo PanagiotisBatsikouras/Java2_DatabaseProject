a525f34762950975e19736bb052850a645b191ec
PanagiotisBatsikouras <t8160079@dias.aueb.gr>
2019-01-06 22:56:57 +0200

    message change

--- dbproject/src/main/java/javameister/dbproject/Deletion.java
+++ dbproject/src/main/java/javameister/dbproject/Deletion.java
@@ -13,11 +13,14 @@ public class Deletion {
      * based on an unique auto-generated code.
      */
     public static void deleteData() {
-        System.out.println("this is our deletion class");
+
         Scanner sc = new Scanner(System.in);
+
         Database.printer();
+
         System.out.println(
                 "Please select the code " + "of the data you wish to delete ");
+
         String x = (" " + sc.nextInt());
         for (int i = 0; i < Database.getaList().size(); i++) {
             if (Database.getaList().get(i).contains(x)) {
@@ -25,6 +28,8 @@ public class Deletion {
             }
 
         }
+        System.out.println("End of deletion process.");
+
     }
 
 }
