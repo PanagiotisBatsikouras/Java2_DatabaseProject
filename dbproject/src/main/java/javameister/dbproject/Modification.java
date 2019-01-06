28eb7d033ed43b45ad7bc31bbdbe76cce5a73749
cmpaka <t8170077@dias.aueb.gr>
2019-01-06 15:07:29 +0200

    Update Modification.java

--- dbproject/src/main/java/javameister/dbproject/Modification.java
+++ dbproject/src/main/java/javameister/dbproject/Modification.java
@@ -1,10 +1,35 @@
 package javameister.dbproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Javameister.
  *
  */
 class Modification {
-
+	
+	public static void changeData() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Database.printer();
+		System.out.println("Please select the code of the data you wish to change ");
+		String x= (" " + sc.nextInt());
+		Database.printer2();
+		int y = 0;
+		System.out.println("Please select the number of the field that you wish to change ");
+		y = sc.nextInt();
+		String el;
+		System.out.println("Please enter the new element ");
+		el = sc.next().toLowerCase();
+		for (int i = 0; i < Database.getaList().size(); i++) {
+			
+			if (Database.getaList().get(i).contains(x)) {
+				 Database.getaList().get(i).set(y, el);
+				 System.out.println("Data successfully changed!");
+			}
+			
+		}
+	}
 }
