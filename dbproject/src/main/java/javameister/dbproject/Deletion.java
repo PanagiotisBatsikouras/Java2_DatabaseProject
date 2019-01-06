0012fe2a311c75ae438854c055ae26bd2208c4f3
AdamAvrantinis <t8170001@dias.aueb.gr>
2019-01-06 16:03:00 +0200

    Deletion Class
    Διαγραφή δεδομένων

--- dbproject/src/main/java/javameister/dbproject/Deletion.java
+++ dbproject/src/main/java/javameister/dbproject/Deletion.java
@@ -5,6 +5,22 @@ package javameister.dbproject;
  * @author Javameister.
  *
  */
-class Deletion {
+import java.util.Scanner;
+
+public class Deletion {
+	
+	public static void deleteData() {
+		System.out.println("this is our deletion class");
+		Scanner sc = new Scanner(System.in);
+		Database.printer();
+		System.out.println("Please select the code of the data you wish to delete ");
+		String x = (" " +sc.nextInt());
+		for (int i = 0; i < Database.getaList().size(); i++) {
+			if (Database.getaList().get(i).contains(x)) {
+				 Database.getaList().remove( Database.getaList().get(i));
+			}
+			
+		}
+	}
 
 }
